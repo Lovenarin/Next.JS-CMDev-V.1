3bc91754c965b0d2d3c6c4d9bd486fa46a2fc1c0
Lovenarin <narin_love_za1@hotmail.com>
2023-01-26 14:53:07 +0700

    asd

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,9 +1,20 @@
 import React from "react";
+import styles from "../styles/alert.module.css";
 
 export default function Aboutus() {
   return (
     <div>
       <h1>Aboutus</h1>
+      <div className={styles.error}>Help Me</div>
+
+      {/* <style jsx>
+        {`
+          .error {
+            background-color: red;
+            color: white;
+          }
+        `}
+      </style> */}
     </div>
   );
 }
